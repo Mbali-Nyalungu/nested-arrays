2b9a2a473131f769d5c796d06baf3ed2aca61088
Mbali-Nyalungu <Mbali.Nyalungu04@gmail.com>
2024-06-19 11:46:34 +0200

    added array

---
+++ index.js
@@ -0,0 +1,4 @@
+const colleagues = [
+    ["Jack","Yonela","Nthabi", "Jade"," Mbali"], [20,24,27,29,25],["bmw"," mercedes","polo","jeep", "mercedes"]["Soweto","Brixton","Randburg","Mamelodi"," Evander"]
+]
+
